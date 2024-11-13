3510e61dc1525edb45db575b3a9ad3ce82a7c5ef
AfeezTemitope <belloafeez28@gmail.com>
2024-11-13 20:35:21 +0100

    updated requirements and used the online db

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
+import app
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    SQLALCHEMY_DATABASE_URI = ('postgresql://neondb_owner:eJpC9dIcNb4h@ep-frosty-snow-a5z0mc27.us-east-2.aws.neon.tech'
+                               '/neondb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
